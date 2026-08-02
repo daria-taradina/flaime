99d69a8fce87a1d9ae5138ef1537e2f5a5062316
daria-taradina <108838898+daria-taradina@users.noreply.github.com>
2026-08-01 23:36:10 -0700

    update about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import { AboutHero } from '@/sections/about/AboutHero';
 import { AboutHero2 } from '@/sections/about/AboutHero2';
+import { AboutHero3 } from '@/sections/about/AboutHero3';
 
 export default function Home() {
   return (
     <main>
+      
       <AboutHero />
-      <AboutHero2 />
+
     </main>
   );
 }
