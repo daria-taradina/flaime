48dd20c6d20bcab2b2e18457ee7a9d2d4d6a2616
daria-taradina <108838898+daria-taradina@users.noreply.github.com>
2026-08-01 23:39:15 -0700

    fix about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,4 @@
 import { AboutHero } from '@/sections/about/AboutHero';
-import { AboutHero2 } from '@/sections/about/AboutHero2';
-import { AboutHero3 } from '@/sections/about/AboutHero3';
 
 export default function Home() {
   return (
